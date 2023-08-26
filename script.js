22cb698a0c463a90adba4806d130c8b1d142746c
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2023-08-27 09:53:59 +1200

    delete button added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 const bookBtn = document.getElementById("book-btn")
+const bookModal = document.getElementById('book-modal')
+const modalForm = document.getElementById('modal-content');
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -9,28 +11,59 @@ function Book(title, author, pages, read) {
 }
 
 bookBtn.addEventListener('click', function() {
-  const titleInput = prompt("Enter title")
-  const authorInput = prompt("Enter Author")
-  const pagesInput = prompt("Enter pages")
-  const readInput = prompt("Have you read it? yes or no")
-  const newBook = new Book(titleInput, authorInput, pagesInput, readInput)
+  bookModal.style.display = 'block'
+})
+
+modalForm.addEventListener('submit', function(event) {
+
+  event.preventDefault()
+
+  const titleInput = document.getElementById('title-input')
+  const authorInput = document.getElementById('author-input')
+  const pagesInput = document.getElementById('pages-input')
+  const readInput = document.getElementById('read-input')
+
+  const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value)
   myLibrary.push(newBook)
   displayLibrary()
+   
+   titleInput.value = '';
+   authorInput.value = '';
+   pagesInput.value = '';
+   readInput.value = '';
+
+   bookModal.style.display = 'none';
 })
 
 function displayLibrary() {
-  const booksContainer = document.getElementById('books-container')
-  booksContainer.innerHTML = ''
 
-  myLibrary.forEach(book => {
+  const booksContainer = document.getElementById('books-container')
+    booksContainer.innerHTML = ''
+
+  myLibrary.forEach((book, index) => {
+
     const bookCard = document.createElement('div')
     bookCard.setAttribute('id', 'book-card')
-booksContainer.append(bookCard)
 
     bookCard.innerHTML = 
-`Title: ${book.title} <br> Author: ${book.author} 
-<br> Page count: ${book.pages} <br> Read: ${book.read}`
-  })
+      `Title: ${book.title} <br> Author: ${book.author} 
+      <br> Page count: ${book.pages} <br> Read: ${book.read}
+      <button class="deleteBtn" data-index="${index}">Delete</button>
+      <button>Read</button>`
 
-}
+const deleteBtn = bookCard.querySelector('.deleteBtn')
+
+deleteBtn.addEventListener('click', function() {
+  const bookIndex = parseInt(this.getAttribute('data-index'));
+  myLibrary.splice(bookIndex, 1);
+  displayLibrary();
+})
+
+
+    booksContainer.append(bookCard)
+
+    })
+  }
+
+
 
